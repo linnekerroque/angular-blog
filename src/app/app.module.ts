c315afcf4758127266e6e1b6ae0d60e105aa8187
LinnekerRoque <linneker@outlook.com>
2022-09-20 15:12:50 -0300

    Bootstrap, Components and Pages Add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { CardComponent } from './components/card/card.component';
+import { HomeComponent } from './pages/home/home.component';
+import { PostComponent } from './pages/post/post.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    CardComponent,
+    HomeComponent,
+    PostComponent
   ],
   imports: [
     BrowserModule,
