d45233269809034b3a1957fdbeb20fe279315b77
LinnekerRoque <linneker@outlook.com>
2022-09-27 14:51:54 -0300

    Content Post Add

--- src/app/pages/post/post.component.ts
+++ src/app/pages/post/post.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { BlogDataService } from '../../blog-data.service'
 
 @Component({
   selector: 'app-post',
@@ -7,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostComponent implements OnInit {
 
-  constructor() { }
+  postTitle:string = '';
+  postBody:string = '';
+  postImg:string = '';
+
+  private postId: string = '';
+
+  constructor(private postBlog:BlogDataService, private route:ActivatedRoute) {
+    this.route.params.subscribe(
+      res => this.postId = res['id']
+    );
+  }
 
   ngOnInit(): void {
+    const post = this.postBlog.getPosts().filter(post => post.id == this.postId)[0];
+    this.postTitle = post.title;
+    this.postBody = post.body;
+    this.postImg = post.img;
   }
 
 }
